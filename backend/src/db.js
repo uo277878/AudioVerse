689f6e42024c9a8b184eebc8e90e9e1b0bd5c6fa
uo277878 <uo277878@uniovi.es>
2024-02-07 17:52:56 +0100

    Conexion a BBDD fuciona

---
+++ backend/src/db.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+export const connectDb = async() => {
+    try{
+        await mongoose.connect('mongodb+srv://uo277878:ingenieriainformatica2001@cluster0.hjchjtv.mongodb.net/?retryWrites=true&w=majority');
+        console.log("DB connected")
+    } catch(error){
+        console.log(error);
+    }
+};
