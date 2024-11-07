83657684105d9737baed9ca7f1b516aae917d2a8
uo277878 <uo277878@uniovi.es>
2024-11-08 00:22:10 +0100

    No funciona backend

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -25,6 +25,7 @@ export const AuthProvider = ({children}) => {
             setUser(res.data);
             setIsAuthenticated(true);
         } catch(error){
+            console.log(error);
             if(Array.isArray(error.response.data)){
                 return setErrors(error.response.data);
             }
