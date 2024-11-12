007b8f0c0e5bd6a17ba5e06852eb133058f988ae
uo277878 <uo277878@uniovi.es>
2024-11-13 00:03:19 +0100

    Arreglada comunicacion front-back

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,14 @@ import morgan from 'morgan'
 import cookieParser from 'cookie-parser';
 import authRouters from './routes/auth.js'
 import usersRouters from './routes/users.js'
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
